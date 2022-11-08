82ab0e60c1bb4a64650ac79734673b76a0ede4ee
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-11-08 17:34:36 +0700

    modulation

---
+++ modulation/Tiger.js
@@ -0,0 +1,11 @@
+class Tiger {
+    constructor(){
+        this.strength = Math.floor(Math.random() * 100);
+    }
+
+    growl(){
+        console.log("grrrrrrr!");
+    }
+}
+
+module.exports = Tiger;
